434c802b1f70e35c930a848558538e76615aea0f
suhymin97 <mity.2a@gmail.com>
2018-03-26 00:15:07 +0700

    Edit Pet and Pets

--- app/src/main/java/dsa/hcmiu/a2048pets/entities/model/Pet.java
+++ app/src/main/java/dsa/hcmiu/a2048pets/entities/model/Pet.java
@@ -5,6 +5,4 @@ package dsa.hcmiu.a2048pets.entities.model;
  */
 
 public interface Pet {
-    public int checkMatch( int n2);
-    public int explode( int id2, int id3);
 }
--- app/src/main/java/dsa/hcmiu/a2048pets/entities/model/Pets.java
+++ app/src/main/java/dsa/hcmiu/a2048pets/entities/model/Pets.java
@@ -38,26 +38,4 @@ public class Pets implements Pet {
         pic = p.getPic();
     }
 
-
-    @Override
-    public int checkMatch(int id2) {
-
-        if (this.id == id2)
-            return 1;
-        else return -1;
-    }
-
-    @Override
-    public int explode(int id2, int id3) {
-        if (this.checkMatch(id2) == 1 && this.checkMatch(id3) == 1) {
-            score += 1;
-            return 1;
-        }
-        return -1;
-    }
-
-    public int crush(int id) {
-        this.id = 100;
-        return this.id;
-    }
 }
