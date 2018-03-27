f949c0a6cb979a4d18712287e7f7b60636630590
suhymin97 <mity.2a@gmail.com>
2018-03-27 16:33:34 +0700

    Change from 2D matrix to arraylist; Add Stack as history of movement; Edit board and HandleGame class to work with arraylist and Stack.

--- app/src/main/java/dsa/hcmiu/a2048pets/entities/handle/HandleGame.java
+++ app/src/main/java/dsa/hcmiu/a2048pets/entities/handle/HandleGame.java
@@ -1,6 +1,7 @@
 package dsa.hcmiu.a2048pets.entities.handle;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Stack;
 
 import dsa.hcmiu.a2048pets.entities.model.Board;
@@ -14,48 +15,87 @@ public class HandleGame {
     ArrayList<Integer> List, ListUndo, ListHighScore;
     ArrayList<Integer> List1, List2;
     public static int highScore = 0;
+    private static HandleGame instance;
+    public static int countUndo;
     public static int best = 0;
-    private Stack<int[][]> move = new Stack<>();
+    private static int score =0;
+    private int countEmpty=0;
+    public Board curBoard;
+    private Stack<Board> boardStack = new Stack<>();
     int row;
     int col;
     int countMove;
 
-    public HandleGame() {
-        Board.getInstance();
-        getBest();
+    private Random random = new Random();
+
+    public static HandleGame getInstance() {
+        if (instance == null) {
+            instance = new HandleGame();
+        }
+        return instance;
     }
 
-    public void saveBoard() {
-        List = new ArrayList<Integer>();
-        for (row = 0; row < Board.max; row++) {
-            for (col = 0; col < Board.max; col++) {
-                List.add(Board.getInstance().getElement(row, col));
+    private HandleGame() {
+        newGame();
+    }
+
+    private void init() {
+        score = 0;
+        countUndo = 0;
+        int rCol1 = random.nextInt(Board.max-1);
+        int rRow1 = random.nextInt(Board.max-1);
+        int rCol2 = random.nextInt(Board.max-1);
+        int rRow2 = random.nextInt(Board.max-1);
+        curBoard = new Board(0);
+        curBoard.setElement(rRow1,rCol1,2);
+        curBoard.setElement(rRow2,rCol2,(random.nextInt(1)+1)*2);
+    }
+
+    public void newGame() {
+        boardStack.clear();
+        init();
+    }
+
+    public void saveHis() {
+        boardStack.push(curBoard);
+    }
+
+    public void addRandomNumber(){
+        if(countEmpty != 0)
+        {
+            int addPosition = random.nextInt(countEmpty);
+            int count0 = 0 ;
+            for (row = 0 ; row < Board.max; row++)
+            {
+                for (col = 0; col < Board.max; col ++)
+                {
+                    if (count0 == addPosition) //+1
+                    {
+                        curBoard.setElement(row,col,(random.nextInt(1)+1)*2);
+                    }
+                    if (curBoard.getElement(row,col) == 0) count0++;
+                }
             }
         }
     }
 
-    public void Undo() {
+    public boolean Undo() {
         List1 = new ArrayList<Integer>();
         List2 = new ArrayList<Integer>();
         int countChange = 0;
 
-        for (row = 0; row < Board.max; row++) {
-            for (col = 0; col < Board.max; col++) {
-                List1.add(Board.getInstance().getElement(row, col));
-            }
-        }
-        if (Board.countUndo < 3 && Board.score > 100) {
-            for (int row = 0; row < Board.max; row++) {
-                for (int col = 0; col < Board.max; col++) {
-                    Board.getInstance().setElement(row, col, ListUndo.get(row * Board.max + col));
+        if (countUndo==5) return false;
+        saveBoard();
+        for (int row = 0; row < Board.max; row++) {
+            for (int col = 0; col < Board.max; col++) {
+                curBoard.setElement(row, col, ListUndo.get(row * Board.max + col));
                 }
             }
-            Board.score = Board.scoreUndo;
-        }
+            curBoard.score = curBoard.scoreUndo;
 
         for (row = 0; row < Board.max; row++) {
             for (col = 0; col < Board.max; col++) {
-                List2.add(Board.getInstance().getElement(row, col));
+                List2.add(curBoard.getElement(row, col));
             }
         }
 
@@ -68,103 +108,101 @@ public class HandleGame {
         if (countChange == List1.size()) {
             countChange = 0;
         } else {
-            Board.countUndo++;
+            countUndo++;
         }
-
     }
 
     public void pushUp() {
+        countEmpty=0;
         for (col = 0; col < Board.max; col++) {
             int count0 = 0;
             for (row = 0; row < Board.max; row++) {
-                if (Board.getInstance().getElement(row, col) == 0) count0++;
+                if (curBoard.getElement(row, col) == 0) count0++;
                 else {
-                    int t = Board.getInstance().getElement(row, col);
-                    Board.getInstance().setElement(row + count0, col, t);
-                    Board.getInstance().setElement(row, col, 0);
+                    int t = curBoard.getElement(row, col);
+                    curBoard.setElement(row + count0, col, t);
+                    curBoard.setElement(row, col, 0);
                     countMove++;
                 }
             }
+            countEmpty+=count0;
         }
     }
 
     public void moveUp() { //3-1
         countMove=0;
-        saveBoard();
+        saveHis();
         pushUp();
         for (col = 0; col < Board.max; col++) {
             for (row = 1; row < Board.max; row++) {
-                if (Board.getInstance().getElement(row, col) == Board.getInstance().getElement(row - 1, col)) {
-                    int t = Board.getInstance().getElement(row, col);
-                    Board.getInstance().setElement(row - 1, col, t*t);
-                    Board.getInstance().setElement(row, col, 0);
-                    Board.score += Board.getInstance().getElement(row-1, col);
+                if (curBoard.getElement(row, col) == curBoard.getElement(row - 1, col)) {
+                    int t = curBoard.getElement(row, col);
+                    curBoard.setElement(row - 1, col, t*t);
+                    curBoard.setElement(row, col, 0);
+                    curBoard.score += curBoard.getElement(row-1, col);
                 }
             }
         }
         pushUp();
         if (countMove>0){
             countMove = 0;
-            Board.getInstance().addRandomNumber();
-            ListUndo.clear();
-            ListUndo.addAll(List);
-            Board.scoreUndo = Board.score - 100;
+            addRandomNumber();
         }
     }
 
     public void pushDown() { //row
-        int count0;
+        countEmpty=0;
         for (col = 0; col < Board.max; col++) {
-            count0 = 0;
+            int count0 = 0;
             for (row = Board.max - 1; row >= 0; row--) {
-                if (Board.getInstance().getElement(row, col) == 0) count0++;
+                if (curBoard.getElement(row, col) == 0) count0++;
                 else {
-                    int t = Board.getInstance().getElement(row, col);
-                    Board.getInstance().setElement(row + count0, col, t);
-                    Board.getInstance().setElement(row, col, 0);
+                    int t = curBoard.getElement(row, col);
+                    curBoard.setElement(row + count0, col, t);
+                    curBoard.setElement(row, col, 0);
                     countMove++;
                 }
             }
+            countEmpty+=count0;
         }
     }
 
     public void moveDown() { //0-3
         countMove=0;
-        saveBoard();
+        saveHis();
         pushUp();
 
         for (col = 0; col < Board.max; col++) {
             for (row = Board.max - 2; row >= 0; row--) {
-                if (Board.getInstance().getElement(row, col) == Board.getInstance().getElement(row + 1, col)) {
-                    int t = Board.getInstance().getElement(row + 1, col);
-                    Board.getInstance().setElement(row + 1, col, t*t);
-                    Board.getInstance().setElement(row, col, 0);
-                    Board.score += Board.getInstance().getElement(row + 1, col);
+                if (curBoard.getElement(row, col) == curBoard.getElement(row + 1, col)) {
+                    int t = curBoard.getElement(row + 1, col);
+                    curBoard.setElement(row + 1, col, t*t);
+                    curBoard.setElement(row, col, 0);
+                    curBoard.score += curBoard.getElement(row + 1, col);
                 }
             }
         }
         pushDown();
         if (countMove >0 ) {
-            Board.getInstance().addRandomNumber();
-            ListUndo.clear();
-            ListUndo.addAll(List);
-            countMove = 0;
-            Board.scoreUndo = Board.score - 100;
+            countMove=0;
+            addRandomNumber();
         }
     }
 
     public void pushRight() {
+        countEmpty=0;
             for (row = 0; row < Board.max; row++) {
                 int count0 = 0;
                 for (col = Board.max-2; col >=0; col--) {
-                    if (Board.getInstance().getElement(row, col) == 0) count0++;
+                    if (curBoard.getElement(row, col) == 0) count0++;
                     else {
-                        int t = Board.getInstance().getElement(row, col);
-                        Board.getInstance().setElement(row, col + count0, t);
-                        Board.getInstance().setElement(row, col, 0);
+                        int t = curBoard.getElement(row, col);
+                        curBoard.setElement(row, col + count0, t);
+                        curBoard.setElement(row, col, 0);
                         countMove++;
                     }
                 }
+                countEmpty+=count0;
             }
         }
 
@@ -175,72 +213,68 @@ public class HandleGame {
 
         for (row = 0; row < Board.max; row++) {
             for (col = Board.max - 2; col >= 0; col--) {
-                if (Board.getInstance().getElement(row, col) == Board.getInstance().getElement(row, col + 1)) {
-                    int t = Board.getInstance().getElement(row, col + 1);
-                    Board.getInstance().setElement(row, col + 1, t*t);
-                    Board.getInstance().setElement(row, col, 0);
-                    Board.score += Board.getInstance().getElement(row, col + 1);
+                if (curBoard.getElement(row, col) == curBoard.getElement(row, col + 1)) {
+                    int t = curBoard.getElement(row, col + 1);
+                    curBoard.setElement(row, col + 1, t*t);
+                    curBoard.setElement(row, col, 0);
+                    curBoard.score += curBoard.getElement(row, col + 1);
                 }
             }
         }
         pushRight();
         if (countMove >0 ) {
-            Board.getInstance().addRandomNumber();
-            ListUndo.clear();
-            ListUndo.addAll(List);
-            countMove = 0;
-            Board.scoreUndo = Board.score - 100;
+            countMove=0;
+            addRandomNumber();
         }
     }
 
     public void pushLeft() {
+        countEmpty=0;
         for (row = 0; row < Board.max; row++) {
             int count0 = 0;
             for (col = 0; col <Board.max; col++) {
-                if (Board.getInstance().getElement(row, col) == 0) count0++;
+                if (curBoard.getElement(row, col) == 0) count0++;
                 else {
-                    int t = Board.getInstance().getElement(row, col);
-                    Board.getInstance().setElement(row, col - count0, t);
-                    Board.getInstance().setElement(row, col, 0);
+                    int t = curBoard.getElement(row, col);
+                    curBoard.setElement(row, col - count0, t);
+                    curBoard.setElement(row, col, 0);
                     countMove++;
                 }
             }
+            countEmpty+=count0;
         }
     }
 
     public void moveLeft() { //3-0
         countMove=0;
         saveBoard();
-        pushUp();
+        pushLeft();
         for (row = 0; row < Board.max; row++) {
             for (col = 1; col < Board.max; col++) {
-                if (Board.getInstance().getElement(row, col) == Board.getInstance().getElement(row, col - 1)) {
-                    int t = Board.getInstance().getElement(row, col);
-                    Board.getInstance().setElement(row, col - 1, t*t);
-                    Board.getInstance().setElement(row, col, 0);
-                    Board.score += Board.getInstance().getElement(row, col-1);
+                if (curBoard.getElement(row, col) == curBoard.getElement(row, col - 1)) {
+                    int t = curBoard.getElement(row, col);
+                    curBoard.setElement(row, col - 1, t*t);
+                    curBoard.setElement(row, col, 0);
+                    curBoard.score += curBoard.getElement(row, col-1);
                 }
             }
         }
-        pushUp();
+        pushLeft();
         if (countMove>0){
             countMove = 0;
-            Board.getInstance().addRandomNumber();
-            ListUndo.clear();
-            ListUndo.addAll(List);
-            Board.scoreUndo = Board.score - 100;
+            addRandomNumber();
         }
     }
 
     public boolean gameOver() {
-        return Board.getInstance().fullCol() && Board.getInstance().fullRow() && Board.getInstance().fullSpecial();
+        return curBoard.fullCol() && curBoard.fullRow() && curBoard.fullSpecial();
     }
 
     public void saveBest() {
         ListHighScore = new ArrayList<Integer>();
-        if (Board.score > best) {
-            highScore = Board.score;
-            best = Board.score;
+        if (curBoard.score > best) {
+            highScore = curBoard.score;
+            best = curBoard.score;
         } else {
             highScore = best;
         }
--- app/src/main/java/dsa/hcmiu/a2048pets/entities/model/Board.java
+++ app/src/main/java/dsa/hcmiu/a2048pets/entities/model/Board.java
@@ -1,121 +1,57 @@
 package dsa.hcmiu.a2048pets.entities.model;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 /**
  * Created by Admin on 3/25/2018.
  */
 
 public class Board {
-    private static Board instance;
-    public static int row;
-    public static int col;
-    public static int score;
-    public static int countUndo;
-    public static int scoreUndo;
+    private int scoreBoard;
     public static int max = 4;
-    private static int[][] board = new int[max][max];
-    private Random random = new Random();
-    private ArrayList<Integer> List1, List2;
+    private ArrayList<Integer> matrix;
+    int row,col;
 
-
-    private Board() {
-        init();
+    private ArrayList<Integer> initBoard() {
+        ArrayList<Integer> init = new ArrayList<>();
+        for(int i = 0; i<max*max; i++) {
+            init.add(0);
+        }
+        return init;
     }
 
-    public static Board getInstance() {
-        if (instance == null) {
-            instance = new Board();
-        }
-        return instance;
+    public Board(int scoreBoard, ArrayList<Integer> boardi) {
+        this.scoreBoard = scoreBoard;
+        if (!matrix.isEmpty()) matrix.clear();
+        this.matrix.addAll(matrix);
+    }
+
+    public Board(int scoreBoard) {
+        if (matrix.isEmpty()) matrix.addAll(initBoard());
+        this.scoreBoard = scoreBoard;
     }
 
     public int getElement(int rowi, int col) {
-        return board[rowi][col];
+        return matrix.get(rowi*4+col);
     }
 
     public void setElement(int rowi, int col, int value) {
-        board[rowi][col]=value;
+        matrix.set(rowi*4+col,value);
     }
 
-/*	public static void printboard() {
-		for (int i=0;i<20;i++) {
-			for(int k=0;k<8;k++) {
-				System.out.print(board[i][k]+"\t");
-			}
-			System.out.println("\n");
-		}
-	}*/
-
-    private void init() {
-        score = 0;
-        countUndo = 0;
-        scoreUndo = 0;
-        int rCol1 = random.nextInt(max);
-        int rRow1 = random.nextInt(max);
-        int rCol2 = random.nextInt(max-rCol1);
-        int rRow2 = random.nextInt(max-rRow1);
-        for (row =0; row <max; row++)
-        {
-            for(col = 0; col <max; col++)
-            {
-                board[rCol1][rRow1] = 2;
-                board[rCol2][rRow2] = (random.nextInt(2)+1)*2;
-            }
-        }
+    public int getScoreBoard() {
+        return scoreBoard;
     }
 
-    public void newGame() {
-        for (col = 0; col < max; col++) {
-            for (row = 0; row < max; row++) {
-                board[col][row] = 0;
-            }
-        }
-        init();
+    public void setScoreBoard(int scoreBoard) {
+        this.scoreBoard = scoreBoard;
     }
-
-    public void addRandomNumber(){
-        int countEmpty = 0;
-        for (row = 0; row < max; row++)
-        {
-            for (col = 0; col < max; col++)
-            {
-                if (board[col][row] == 0)
-                {
-                    countEmpty ++;
-                }
-            }
-        }
-
-        if(countEmpty != 0)
-        {
-            int addedPosition = random.nextInt(countEmpty);
-            int countPosition = 0 ;
-            for (row = 0 ; row < max; row++)
-            {
-                for (col = 0; col < max; col ++)
-                {
-                    if (board[col][row] == 0)
-                    {
-                        countPosition++;
-                    }
-                    if (countPosition == addedPosition + 1)
-                    {
-                        board[col][row] =(random.nextInt(2)+1)*2;
-                        return;
-                    }
-                }
-            }
-        }
-    }
-
 
     public boolean fullRow(){
         int count = 0;
         for(row= 0; row <max-1;row++) {
             for (col = 0; col < max; col++) {
-                if(board[row][col] == 0 || board[row][col] == board[row+1][col] )
+                if(matrix[row][col] == 0 || matrix[row][col] == matrix[row+1][col] )
                 {
                     count++;
                 }
@@ -129,7 +65,7 @@ public class Board {
         int count=0;
         for(row= 0; row <max;row++) {
             for (col = 0; col < max-1; col++) {
-                if (board[row][col] == 0 || board[row][col] == board[row][col + 1]) {
+                if (matrix[row][col] == 0 || matrix[row][col] == matrix[row][col + 1]) {
                     count++;
                 }
             }
@@ -141,7 +77,7 @@ public class Board {
     public boolean fullSpecial()
     {
         boolean check = false;
-        if(board[3][3] == 0 || board[3][3] == board[3][2]|| board[3][3] == board[2][3] ) return false;
+        if(matrix[3][3] == 0 || matrix[3][3] == matrix[3][2]|| matrix[3][3] == matrix[2][3] ) return false;
         else return true;
     }
 
