c43d4736430b92b352b5c9d6e7f55a26c68f09dc
DELL <buingoc.thanhhien97@gmail.com>
2018-03-29 22:03:11 +0700

    + New class    - itemAdapter   ( = PetAdapter )    - data    - square        ( = Pet)    - MainActivity
    + New xml:
       - activity_main   ( UI )
       - item_square   ( = item_Pet)
    + New color
       - ItemColor    ( in 'colors' )

---
+++ app/src/main/java/dsa/hcmiu/a2048pets/MainActivity.java
@@ -0,0 +1,38 @@
+package dsa.hcmiu.a2048pets;
+
+import dsa.hcmiu.a2048pets.entities.adapter.itemAdapter;
+import dsa.hcmiu.a2048pets.entities.model.data;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.GridView;
+
+
+
+public class MainActivity extends Activity{
+    //public class MainActivity extends AppCompatActivity {
+    private GridView gameplay;
+    private itemAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        create();
+        show();
+        setData();
+    }
+
+    private void create(){
+        data.getDatagame().value16(MainActivity.this);
+        adapter = new itemAdapter(MainActivity.this,0, data.getDatagame().getnItems());
+    }
+
+    private void show(){
+        gameplay = (GridView)findViewById(R.id.gameplay);
+    }
+
+    private void setData(){
+        gameplay.setAdapter(adapter);
+    }
+}
+
---
+++ app/src/main/java/dsa/hcmiu/a2048pets/entities/model/data.java
@@ -0,0 +1,122 @@
+package dsa.hcmiu.a2048pets.entities.model;
+
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import dsa.hcmiu.a2048pets.R;
+
+// Design pattern //
+public class data {
+    public static data datagame;
+
+    //Array1D: color for squares //
+    private int[] Array1D_color;
+
+    // ArrayList: display the number of items //
+    private ArrayList<Integer> nItems = new ArrayList<>();
+
+    // Array2D: better to manage the items (4x4) //
+    private int[][] Array2D = new int[4][4];
+
+    //Random number//
+    private Random random = new Random();
+
+
+
+    // Constructor //
+    static{
+        datagame = new data();
+    }
+    public static data getDatagame(){
+        return datagame;
+    }
+
+
+    // Method //
+    public void value16(Context context) {
+        int i, j;
+
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 4; j++) {
+                Array2D[i][j] = 0; // Set current value //
+                nItems.add(0);
+            }
+        }
+        // TypedArray: link to the file "res" //
+        TypedArray rescolor = context.getResources().obtainTypedArray(R.array.ItdemColor); //Take the color in "drawable"//
+        Array1D_color = new int[rescolor.length()];  //Get the number of color//
+
+        for (i = 0; i < rescolor.length(); i++) {
+            Array1D_color[i] = rescolor.getColor(i, 0); //getColor (int index, int defValue)//
+        }
+        rescolor.recycle(); //Close the res//
+
+        createNumber();
+        exchange();
+    }
+
+
+    public int color(int n){
+        // If the value is 0, let it WHITE (We only have : 2,4,8,16,32,64,......) //
+        if(n==0){
+            return Color.WHITE;
+        }
+        else{
+            int a = (int) (Math.log(n) / Math.log(2));  //Should have (int) because we have logarit as double//
+            return Array1D_color [a-1];   //Position of color (first color=0) = the result - 1//
+        }
+    }
+
+    public void createNumber(){
+        int n0 = 0;
+        int numbercreate;
+        for(int i=0; i<16; i++){
+            if( nItems.get(i)==0 ){
+                n0++;
+            }
+        }
+
+        if(n0>1){  //If it's full, don't create more Number//
+            numbercreate = random.nextInt(2)+1;
+        }
+        else{
+            if(n0==1){
+                numbercreate = 1;
+            }
+            else{
+                numbercreate = 0;
+            }
+        }
+
+        while(numbercreate!=0){
+            int i = random.nextInt(4);
+            int j = random.nextInt(4);
+
+            if(Array2D[i][j] == 0){
+                Array2D[i][j] = 16;
+                numbercreate--;
+            }
+        }
+    }
+
+    public void exchange() {
+        nItems.clear();
+        int i, j;
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 4; j++) {
+                nItems.add(Array2D[i][j]);
+            }
+        }
+    }
+
+    // Get ArrayList //
+    public ArrayList<Integer> getnItems() {
+        return nItems;
+    }
+}
+
