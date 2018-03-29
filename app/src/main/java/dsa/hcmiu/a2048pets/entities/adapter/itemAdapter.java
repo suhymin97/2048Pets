2a790f40133aed668258e1865fcba90fa7fef768
DELL <buingoc.thanhhien97@gmail.com>
2018-03-29 21:41:19 +0700

    - New class itemAdapter data square MainActivity - New xml: activity_main - New color ItemColor

---
+++ app/src/main/java/dsa/hcmiu/a2048pets/entities/adapter/itemAdapter.java
@@ -0,0 +1,39 @@
+package dsa.hcmiu.a2048pets.entities.adapter;
+
+
+import android.content.Context;
+// Appear when add Constructor //
+
+import android.view.LayoutInflater;
+import android.widget.ArrayAdapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import java.util.ArrayList;
+import java.util.List;
+
+import dsa.hcmiu.a2048pets.R;
+
+
+public class itemAdapter extends ArrayAdapter<Integer> {
+    private Context context;
+    private ArrayList<Integer> array;
+
+
+    public itemAdapter(Context context, int resource, List<Integer> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView==null){  //Check NULL
+            LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE); //Chuyen layout thanh 1 item
+            convertView = inflater.inflate(R.layout.item_square,null);
+        }
+        if(array.size() > 0){
+            square sq = (square)convertView.findViewById(R.id.square); // Take ID from "item_square" //
+            sq.textFormat(array.get(position));
+        }
+        return convertView;
+    }
+}
---
+++ app/src/main/java/dsa/hcmiu/a2048pets/entities/model/square.java
@@ -0,0 +1,66 @@
+package dsa.hcmiu.a2048pets.entities.model;
+
+
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+
+public class square extends TextView {
+
+    //CONSTRUCTOR//
+
+    public square(Context context) {
+        super(context);
+    }
+
+    public square(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public square(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    //  WIDTH - HEIGHT FOR A SQUARE //
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int a = getMeasuredWidth();
+        setMeasuredDimension(a,a);
+    }
+
+    public void textFormat(int n) {
+        if (n < 100) {
+            setTextSize(40);
+        } else if (n < 1000) {
+            setTextSize(35);
+        } else {
+            setTextSize(30);
+        }
+        if (n >= 8) {
+            setTextColor(Color.WHITE);
+        } else {
+            setTextColor(Color.BLACK);
+        }
+
+        GradientDrawable draw = (GradientDrawable) this.getBackground();
+        draw.setColor(data.getDatagame().color(n));
+        setBackground(draw);
+
+
+        if (n==0){
+            setText(" ");
+        }
+        else{
+            setText("" + n);
+        }
+    }
+}
+
